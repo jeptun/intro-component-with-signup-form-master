09faf9efdafc9a010b2b250b3037263d765feeb2
jeptun <jeptun@gmail.com>
2022-08-18 20:59:44 +0200

    finish

--- app/js/script.js
+++ app/js/script.js
@@ -21,13 +21,21 @@ const password = document.getElementById("password");
 const firstNameLabel = document.getElementById("first-name-label");
 const firstNameAlert = document.getElementById("first-name-alert");
 
+const lastNameLabel = document.getElementById("last-name-label");
+const lastNameAlert = document.getElementById("last-name-alert");
+
+const emailLabel = document.getElementById("email-label");
+const emailAlert = document.getElementById("email-alert");
+
+const passwordLabel = document.getElementById("password-label");
+const passwordAlert = document.getElementById("password-alert");
+
 firstName.addEventListener("keyup", () => {
   if (firstName.value == "") {
     firstNameLabel.style.display = "none";
     firstNameAlert.removeAttribute("hidden");
     firstName.style.outline = "1px solid #f00";
-  }
-   else if (firstName.value.length > 0) {
+  } else if (firstName.value.length > 0) {
     firstNameAlert.setAttribute("hidden", "");
     firstNameLabel.style.display = "none";
     firstName.style.outline = "0";
@@ -37,5 +45,51 @@ firstName.addEventListener("keyup", () => {
     firstNameAlert.removeAttribute("hidden");
     firstName.style.outline = "1px solid #f00";
     firstNameLabel.style.display = "none";
+  }
+});
+
+lastName.addEventListener("keyup", () => {
+  if (lastName.value == "") {
+    lastNameLabel.style.display = "none";
+    lastNameAlert.removeAttribute("hidden");
+    lastName.style.outline = "1px solid #f00";
+  } else if (lastName.value.length > 0) {
+    lastNameAlert.setAttribute("hidden", "");
+    lastNameLabel.style.display = "none";
+    lastName.style.outline = "0";
+  }
+  if (/[0-9]/.test(lastName.value)) {
+    lastNameAlert.removeAttribute("hidden");
+    lastName.style.outline = "1px solid #f00";
+    lastNameLabel.style.display = "none";
+  }
+});
+
+email.addEventListener("keyup", () => {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+    emailAlert.setAttribute("hidden", "");
+    emailLabel.style.display = "none";
+    email.style.outline = "0";
+  } else {
+    emailLabel.style.display = "none";
+    emailAlert.removeAttribute("hidden");
+    email.style.outline = "1px solid #f00";
+  }
+});
+
+password.addEventListener("keyup", () => {
+  if (password.value == "") {
+    passwordLabel.style.display = "none";
+    passwordAlert.removeAttribute("hidden");
+    password.style.outline = "1px solid #f00";
+  } else if (password.value.length > 0) {
+    passwordLabel.style.display = "none";
+    passwordAlert.removeAttribute("hidden");
+    password.style.outline = "1px solid #f00";
+    if (/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password.value)) {
+      passwordAlert.setAttribute("hidden", "");
+      passwordLabel.style.display = "none";
+      password.style.outline = "0";
+    }
   }
 });
